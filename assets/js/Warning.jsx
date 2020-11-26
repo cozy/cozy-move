23e97a988c7d4b5b987a1566834d509ac67b98d8
Bruno Michel <bmichel@menfin.info>
2020-11-26 09:37:14 +0100

    Use translated wording for the warning modal

--- assets/js/Warning.jsx
+++ assets/js/Warning.jsx
@@ -1,32 +1,31 @@
 import React, { useState } from 'react'
+import ReactMarkdown from 'react-markdown'
+import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 import { ConfirmDialog } from  'cozy-ui/transpiled/react/CozyDialogs'
 import Button from 'cozy-ui/transpiled/react/Button'
 
-const Warning = ({button}) => {
+const Warning = ({button, title, content, cancel, confirm}) => {
   const [state, setState] = useState({ opened: false })
   const toggle = () => setState(state => ({...state, opened: !state.opened}))
 
-  const ConfirmDialogActions = () => {
-    return (
-      <>
-        <Button theme="secondary" onClick={toggle} label={'Close Modal'} />
-        <Button theme="danger" label='Do something destructive' onClick={() => alert('click')} />
-      </>
-    )
-  }
+  const { isMobile } = useBreakpoints()
+
+  const Buttons = () => <>
+          <Button theme="secondary" onClick={toggle} label={cancel} />
+          <Button theme="danger" label={confirm} onClick={() => alert('click')} />
+        </>
 
   return <>
     <button className="c-btn" onClick={toggle}>
       <span>{button}</span>
     </button>
       <ConfirmDialog
-        size={'medium'}
         open={state.opened}
         onClose={toggle}
-        title={"Title"}
-        content={"Content"}
-        actions=<ConfirmDialogActions />
-        actionsLayout={'column'}
+        title={title}
+        content=<ReactMarkdown source={content} />
+        actions=<Buttons />
+        actionsLayout={isMobile ? 'column' : 'row'}
       />
   </>
 }
--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,6 @@ import { render } from "react-dom"
 import { BreakpointsProvider } from "cozy-ui/transpiled/react/hooks/useBreakpoints"
 import MuiCozyTheme from "cozy-ui/transpiled/react/MuiCozyTheme"
 import Warning from "./Warning.jsx"
-
 ;(function (w, d) {
   const wrapReact = (element, app) => {
     render(
@@ -22,7 +21,15 @@ import Warning from "./Warning.jsx"
 
   const warning = d.getElementById("react-warning")
   if (warning) {
-    const app = <Warning button={warning.innerText} />
+    const app = (
+      <Warning
+        button={warning.innerText}
+        title={warning.dataset.title}
+        content={warning.dataset.content}
+        confirm={warning.dataset.confirm}
+        cancel={warning.dataset.cancel}
+      />
+    )
     wrapReact(warning.parentElement, app)
   }
 })(window, document)
