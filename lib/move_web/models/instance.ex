cbde354f37656c543dad258a70b619928af9f592
Bruno Michel <bmichel@menfin.info>
2020-11-30 17:23:54 +0100

    Fix a few things

--- lib/move_web/models/instance.ex
+++ lib/move_web/models/instance.ex
@@ -1,10 +1,21 @@
 defmodule MoveWeb.Models.Instance do
-  defstruct [:url, :disk, :quota]
+  # A URL is formatted like https://move.cozy.example/
+  # Disk and quota are numbers (in bytes) but can be nil
+  # A code can only be used with a mail confirmation (on the source thus).
+  # A token has permission for both exporting and importing
+  defstruct [:url, :disk, :quota, :code, :token]
 
   def quota_error(nil, _target), do: false
   def quota_error(_source, nil), do: false
-  def quota_error(source, target), do: source.disk > target.quota
+  # Self-hosted can have unlimited quota
+  def quota_error(_source, %Instance{quota: nil}), do: false
+  # Unknown disk usage may happen and should not block
+  def quota_error(%Instance{disk: nil}, _target), do: false
+  def quota_error(%Instance{disk: taken}, %Instance{quota: available}), do: taken > available
 
   def can_swap(nil, nil), do: false
+  def can_swap(nil, _target), do: true
+  # A token is required for importing (target)
+  def can_swap(%Instance{token: nil}, nil), do: false
   def can_swap(_source, _target), do: true
 end
