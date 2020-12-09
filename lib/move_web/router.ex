482f72d820358a28b770301685c512d26370615a
Bruno Michel <bmichel@menfin.info>
2020-12-09 09:32:15 +0100

    Fix dashboard on dev/test

--- lib/move_web/router.ex
+++ lib/move_web/router.ex
@@ -4,6 +4,22 @@ defmodule MoveWeb.Router do
   # credo:disable-for-next-line
   @csp Application.get_env(:move, __MODULE__, false)[:csp]
 
+  # Enables LiveDashboard only for development
+  #
+  # If you want to use the LiveDashboard in production, you should put
+  # it behind authentication and allow only admins to access it.
+  # If your application does not have an admins-only section yet,
+  # you can use Plug.BasicAuth to set up some basic authentication
+  # as long as you are also using SSL (which you should anyway).
+  if Mix.env() in [:dev, :test] do
+    import Phoenix.LiveDashboard.Router
+
+    scope "/" do
+      pipe_through :browser
+      live_dashboard "/dashboard", metrics: MoveWeb.Telemetry
+    end
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
     plug :fetch_session
@@ -41,21 +57,5 @@ defmodule MoveWeb.Router do
     get "/:side/add", InstanceController, :add
     get "/:side/edit", InstanceController, :edit
     post "/:side", InstanceController, :update
-  end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: MoveWeb.Telemetry
-    end
   end
 end
