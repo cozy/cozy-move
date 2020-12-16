3634182399a1296e24796f83d60bd200f786a561
Bruno Michel <bmichel@menfin.info>
2020-12-16 18:16:55 +0100

    Take screenshots on both mobile and desktop

--- test/support/feature_case.ex
+++ test/support/feature_case.ex
@@ -8,6 +8,13 @@ defmodule MoveWeb.FeatureCase do
       use Wallaby.Feature
 
       import MoveWeb.Router.Helpers
+
+      defp for_session(fun) do
+        {:ok, mobile} = Wallaby.start_session(window_size: [width: 360, height: 740])
+        fun.(mobile, "mobile")
+        {:ok, desktop} = Wallaby.start_session(window_size: [width: 1280, height: 720])
+        fun.(desktop, "desktop")
+      end
     end
   end
 end
