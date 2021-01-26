f3f471e1f9f899dd33f64c3990c53af1282e958e
Bruno Michel <bmichel@menfin.info>
2021-01-26 16:10:21 +0100

    Fix wordings for submit buttons

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Move.MixProject do
   def project do
     [
       app: :move,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
